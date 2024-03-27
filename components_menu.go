5d92d1c20e6cb22183b5e8b099adc3064f699c18
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2024-03-27 14:44:10 +0100

    Add b.MenuAHref

--- components_menu.go
+++ components_menu.go
@@ -1,6 +1,9 @@
 package bulma
 
 import (
+	"io"
+
+	"github.com/maragudk/gomponents"
 	"github.com/maragudk/gomponents/html"
 )
 
@@ -22,6 +25,51 @@ func MenuList(children ...any) Element {
 // MenuEntry creates an entry for a menu list.
 func MenuEntry(children ...any) Element {
 	return Elem(html.Li, children...)
+}
+
+// MenuAHref creates an entry for a menu list, with a link wrapped inside it.
+func MenuAHref(href string, children ...any) Element {
+	return (&menuAhref{href: href}).With(children...)
+}
+
+type menuAhref struct {
+	href          string
+	entryChildren []any
+	aChildren     []any
+}
+
+func (m *menuAhref) With(children ...any) Element {
+	for _, c := range children {
+		switch c := c.(type) {
+		case *ApplyToInner:
+			m.aChildren = append(m.aChildren, c)
+		case *ApplyToOuter:
+			m.entryChildren = append(m.entryChildren, c)
+		case Class:
+			if c == Active {
+				m.aChildren = append(m.aChildren, c)
+			} else {
+				m.entryChildren = append(m.entryChildren, c)
+			}
+		case ColorClass, ExternalClass, ExternalClassesAndStyles, MultiClass, Styles:
+			m.entryChildren = append(m.entryChildren, c)
+		case gomponents.Node:
+			if IsAttribute(c) {
+				m.entryChildren = append(m.entryChildren, c)
+			} else {
+				m.aChildren = append(m.aChildren, c)
+			}
+		case []any:
+			m.With(c...)
+		default:
+			m.aChildren = append(m.aChildren, c)
+		}
+	}
+	return m
+}
+
+func (m *menuAhref) Render(w io.Writer) error {
+	return MenuEntry(AHref(m.href, m.aChildren...), m.entryChildren).Render(w)
 }
 
 // MenuSublist creates a menu sublist.
