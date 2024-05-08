e0b02962135ea6998bea9f642889cc0403495f56
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2024-05-08 20:38:09 +0200

    Add NotificationCloseButton
    Closes #9

--- elements_notification.go
+++ elements_notification.go
@@ -10,3 +10,9 @@ import (
 func Notification(children ...any) e.Element {
 	return e.Div(e.Class("notification"), children)
 }
+
+// NotificationCloseButton adds a "delete" button to a notification,
+// with the adequate javascript in order to close it.
+func NotificationCloseButton() e.Element {
+	return Delete(e.OnClick(JSRemoveThisNotification))
+}
