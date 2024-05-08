bacf773414d5f87d82d833dd1a05ba36fc724401
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2024-05-08 20:55:23 +0200

    Add DuoOpacities and DuoColors

--- fa/duotone.go
+++ fa/duotone.go
@@ -18,10 +18,24 @@ func SecondaryOpacity(value float64) e.Styles {
 	return e.Styles{"--fa-secondary-opacity": strconv.FormatFloat(value, 'f', 2, 64)}
 }
 
+func DuoOpacities(primary, secondary float64) e.Styles {
+	return e.Styles{
+		"--fa-primary-opacity":   strconv.FormatFloat(primary, 'f', 2, 64),
+		"--fa-secondary-opacity": strconv.FormatFloat(secondary, 'f', 2, 64),
+	}
+}
+
 func PrimaryColor(value string) e.Styles {
 	return e.Styles{"--fa-primary-color": value}
 }
 
 func SecondaryColor(value string) e.Styles {
 	return e.Styles{"--fa-secondary-color": value}
+}
+
+func DuoColors(primary, secondary string) e.Styles {
+	return e.Styles{
+		"--fa-primary-color":   primary,
+		"--fa-secondary-color": secondary,
+	}
 }
